c4fcf2d06adc5d07306e198c05758364a1d46bd3
sdoq19 <sdoq19@163.com>
2016-02-26 14:09:46 +0800

    增加数据库连接池比较（c3p0,druid）

---
+++ src/com/resource/connpool/compare/ConnectionManager2.java
@@ -0,0 +1,52 @@
+package com.resource.connpool.compare;
+
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+/**
+ * Druid连接池
+ * @author zhuzhenhao
+ */
+public class ConnectionManager2 {
+	private static ConnectionManager2 instance;
+	private static DruidDataSource dataSource;
+
+	private ConnectionManager2() {
+		dataSource = new DruidDataSource();
+		dataSource.setUsername("zhuaqianmao_test");
+		dataSource.setPassword("zhuaqianmao1qa2ws");
+		dataSource.setUrl("jdbc:mysql://hejiabin.mysql.rds.aliyuncs.com:3306/xiaojinniu_2?"
+				+ "useUnicode=true&characterEncoding=utf-8");
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setMinIdle(1);
+		dataSource.setMaxActive(10);
+		try {
+			dataSource.setFilters("stat");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public static ConnectionManager2 getInstance() {
+		if (instance == null) {
+			instance = new ConnectionManager2();
+		}
+		return instance;
+	}
+	
+	public synchronized final Connection getConnection() {
+		Connection conn = null;
+		try {
+			conn = dataSource.getConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+}
---
+++ src/com/resource/connpool/compare/test.java
@@ -0,0 +1,160 @@
+package com.resource.connpool.compare;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+public class test {
+	
+	public static void main(String[] args) {
+		System.out.println("----------------不使用连接池...----------------");
+		long tTime1 = 0l;
+		for (int i = 0; i < 30; i++) {
+			long btime = System.currentTimeMillis();
+			MysqlDataSource mSource = new MysqlDataSource();
+			mSource.setUser("zhuaqianmao_test");
+			mSource.setPassword("zhuaqianmao1qa2ws");
+			mSource.setURL("jdbc:mysql://hejiabin.mysql.rds.aliyuncs.com:3306/xiaojinniu_2?useUnicode=true&characterEncoding=utf-8");
+			java.sql.Connection connection = null;
+			java.sql.PreparedStatement ps = null;
+			ResultSet rSet = null;
+			try {
+				connection = mSource.getConnection();
+				ps = connection.prepareStatement("select * from xjn_user u where u.id < 10000;");
+				rSet = ps.executeQuery();
+				while(rSet.next()) {
+					
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				if(rSet!=null) {
+					try {
+						rSet.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(ps !=null) {
+					try {
+						ps.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			Long etime = System.currentTimeMillis();
+			long differTime = (etime - btime);
+			tTime1 += differTime;
+			System.out.println("第" + (i + 1) + "次执行花费时间为:"  
+                    + differTime);  
+		}
+		System.out.println("----------------不使用连接池..."+"总耗时："+tTime1+"----------------");
+		
+		
+		System.out.println("----------------使用C3P0连接池...----------------");
+		long tTime2 = 0l;
+		for (int i = 0; i < 30; i++) {
+			long btime = System.currentTimeMillis();
+			ConnectionManager cManager = ConnectionManager.getInstance();
+			Connection connection = cManager.getConnection();
+			java.sql.PreparedStatement ps = null;
+			ResultSet rSet = null;
+			try {
+				ps = connection.prepareStatement("select * from xjn_user u where u.id < 10000;");
+				rSet = ps.executeQuery();
+				while(rSet.next()) {
+					
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				if(rSet!=null) {
+					try {
+						rSet.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(ps !=null) {
+					try {
+						ps.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			Long etime = System.currentTimeMillis();
+			long differTime = (etime - btime);
+			tTime2 += differTime;
+			System.out.println("第" + (i + 1) + "次执行花费时间为:"  
+                    + differTime);  
+		}
+		System.out.println("----------------使用C3P0连接池..."+"总耗时："+tTime2+"----------------");
+		
+		System.out.println("----------------使用Druid连接池...----------------");
+		long tTime3 = 0l;
+		for (int i = 0; i < 30; i++) {
+			long btime = System.currentTimeMillis();
+			ConnectionManager2 cManager = ConnectionManager2.getInstance();
+			Connection connection = cManager.getConnection();
+			java.sql.PreparedStatement ps = null;
+			ResultSet rSet = null;
+			try {
+				ps = connection.prepareStatement("select * from xjn_user u where u.id < 10000;");
+				rSet = ps.executeQuery();
+				while(rSet.next()) {
+					
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				if(rSet!=null) {
+					try {
+						rSet.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(ps !=null) {
+					try {
+						ps.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				if(connection!=null) {
+					try {
+						connection.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			Long etime = System.currentTimeMillis();
+			long differTime = (etime - btime);
+			tTime3 += differTime;
+			System.out.println("第" + (i + 1) + "次执行花费时间为:"  
+                    + differTime);  
+		}
+		System.out.println("----------------使用Druid连接池..."+"总耗时："+tTime3+"----------------");
+		
+	}
+}
