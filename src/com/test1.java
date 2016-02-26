c5656f3040a54cd438829dc4eeb392f781141083
sdoq19 <sdoq19@163.com>
2016-02-26 16:20:44 +0800

    加入到idea测试

---
+++ src/com/test1.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * Created by Administrator on 2016/2/26.
+ */
+public class test1 {
+
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+    }
+}
