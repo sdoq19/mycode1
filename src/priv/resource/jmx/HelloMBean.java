b19691e0846b15b04b093fb776746f3695145883
zhuzh <zhuzh@citytsm.com>
2017-06-21 17:10:43 +0800

    pom更新

---
+++ src/priv/resource/jmx/HelloMBean.java
@@ -0,0 +1,11 @@
+package priv.resource.jmx;
+
+public interface HelloMBean {
+    public String getName();   
+  
+    public void setName(String name);   
+  
+    public void printHello();   
+  
+    public void printHello(String whoName);   
+}  
